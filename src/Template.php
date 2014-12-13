a87067ccd015a2047a4a60cd111a04b8bdc14f3f
ElPepe <aunsoyjoven@hotmail.com>
2014-12-12 18:20:24 -0600

    Typo in Template

--- src/Template.php
+++ src/Template.php
@@ -102,7 +102,7 @@ class Template
 		$view->assign('mainurl:global', $root );
 		$view->assign('currenturl:global', $root . \iframework\Router::script());
 		$view->assign('currenturlnoscript:global', $root . \iframework\Router::script(true));
-		$view->assign('templateurl:global', $root . \iframework\Router::$APP . '/views/' . $template );
+		$view->assign('templateurl:global', $root . \iframework\Router::$APP . '/view/' . $template );
 		$view->assign('templatename:global', $this->_template);
 		$view->assign('title:global', $template);
 		$view->assign('navigation:global', \iframework\Router::$navigation);
